90e34f62e758f0cd83f6fef6ef8de269c2a1cd4a
kasperg3 <kasperg3@rocketmail.com>
2025-02-16 20:34:13 +0100

    added charts

--- rapt-mate/src/App.js
+++ rapt-mate/src/App.js
@@ -4,75 +4,82 @@ import Overview from './components/Overview'; // Adjust path as needed
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-  const [ssid, setSsid] = useState('');
-  const [password, setPassword] = useState('');
-  const [tabIndex, setTabIndex] = useState(0);
+    const [data, setData] = useState(null);
+    const [ssid, setSsid] = useState('');
+    const [password, setPassword] = useState('');
+    const [tabIndex, setTabIndex] = useState(0);
+    const [selectedMetrics, setSelectedMetrics] = useState({
+        gravity_velocity: true,
+        temperature_celsius: true,
+        specific_gravity: true,
+        accel_x: true,
+        accel_y: true,
+        accel_z: true,
+        battery: true,
+    });
 
-  useEffect(() => {
-    const fetchData = () => {
-      fetch('/data')
-        .then(response => response.json())
-        .then(data => setData(data));
+    useEffect(() => {
+        const fetchData = () => {
+          fetch('/data')
+            .then(response => response.json())
+            .then(data => setData(data));
+        };
+        // Fetch data every second
+        const interval = setInterval(fetchData, 1000);
+        fetchData(); // initial fetch
+        // Cleanup interval on component unmount
+        return () => clearInterval(interval);
+      }, []);
+
+    const handleTabChange = (event, newValue) => {
+        setTabIndex(newValue);
     };
 
-    // Fetch data every second
-    const interval = setInterval(fetchData, 1000);
-    fetchData(); // initial fetch
-
-    // Cleanup interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
-
-  const handleTabChange = (event, newValue) => {
-    setTabIndex(newValue);
-  };
-
-  return (
-    <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">RaptMate</Typography>
-        </Toolbar>
-      </AppBar>
-      <Tabs value={tabIndex} onChange={handleTabChange} centered>
-        <Tab label="Overview" />
-        <Tab label="Settings" />
-      </Tabs>
-      <Container>
-        {tabIndex === 0 && (
-          <Paper elevation={3} className="paper">
-            <Overview data={data} />
-          </Paper>
-        )}
-        {tabIndex === 1 && (
-          <Paper elevation={3} className="paper">
-            <Box id="config">
-              <Typography variant="h5">Configuration</Typography>
-              <TextField
-                label="SSID"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                value={ssid}
-                onChange={(e) => setSsid(e.target.value)}
-              />
-              <TextField
-                label="Password"
-                type="password"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button variant="contained" color="primary">Save</Button>
-            </Box>
-          </Paper>
-        )}
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">RaptMate</Typography>
+                </Toolbar>
+            </AppBar>
+            <Tabs value={tabIndex} onChange={handleTabChange} centered>Z
+                <Tab label="Overview" />
+                <Tab label="Settings" />
+            </Tabs>
+            <Container>
+                {tabIndex === 0 && (
+                    <Paper elevation={3} className="paper">
+                        <Overview data={data} selectedMetrics={selectedMetrics} />
+                    </Paper>
+                )}
+                {tabIndex === 1 && (
+                    <Paper elevation={3} className="paper">
+                        <Box id="config">
+                            <Typography variant="h5">Configuration</Typography>
+                            <TextField
+                                label="SSID"
+                                variant="outlined"
+                                fullWidth
+                                margin="normal"
+                                value={ssid}
+                                onChange={(e) => setSsid(e.target.value)}
+                            />
+                            <TextField
+                                label="Password"
+                                type="password"
+                                variant="outlined"
+                                fullWidth
+                                margin="normal"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Button variant="contained" color="primary">Save</Button>
+                        </Box>
+                    </Paper>
+                )}
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- rapt-mate/src/components/Overview.js
+++ rapt-mate/src/components/Overview.js
@@ -1,25 +1,80 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box} from '@mui/material';
+import { LineChart } from '@mui/x-charts/LineChart';
 
+import { useState } from 'react';
+import 'chart.js/auto';
 const Overview = ({ data }) => {
-  return (
-    <Box>
-      <Typography variant="h5">Overview</Typography>
-      {data ? (
-        <>
-          <Box className="data-item">Gravity Velocity: {data.gravity_velocity}</Box>
-          <Box className="data-item">Temperature (Celsius): {data.temperature_celsius}</Box>
-          <Box className="data-item">Specific Gravity: {data.specific_gravity}</Box>
-          <Box className="data-item">Acceleration X: {data.accel_x}</Box>
-          <Box className="data-item">Acceleration Y: {data.accel_y}</Box>
-          <Box className="data-item">Acceleration Z: {data.accel_z}</Box>
-          <Box className="data-item">Battery: {data.battery}</Box>
-        </>
-      ) : (
-        <Typography>Loading...</Typography>
-      )}
-    </Box>
-  );
+    const [selectedMetrics, setSelectedMetrics] = useState({
+        gravity_velocity: true,
+        temperature_celsius: true,
+        specific_gravity: true,
+        accel_x: true,
+        accel_y: true,
+        accel_z: true,
+        battery: true,
+    });
+
+    return (
+        <Box>
+            <Typography variant="h5">Overview</Typography>
+            {data && data.timestamps && data.gravity_velocity && data.temperature_celsius && data.specific_gravity && data.accel_x && data.accel_y && data.accel_z && data.battery ? (
+                <>
+                    <LineChart
+                        xAxis={[{ data: data.timestamps }]}
+                        series={[
+                            {
+                                label: 'Gravity Velocity',
+                                data: data.gravity_velocity,
+                                borderColor: 'rgb(9, 12, 12)',
+                                hidden: !selectedMetrics.gravity_velocity,
+                            },
+                            {
+                                label: 'Temperature (Celsius)',
+                                data: data.temperature_celsius,
+                                borderColor: 'rgba(255,99,132,1)',
+                                hidden: !selectedMetrics.temperature_celsius,
+                            },
+                            {
+                                label: 'Specific Gravity',
+                                data: data.specific_gravity,
+                                borderColor: 'rgba(54,162,235,1)',
+                                hidden: !selectedMetrics.specific_gravity,
+                            },
+                            {
+                                label: 'Acceleration X',
+                                data: data.accel_x,
+                                borderColor: 'rgba(255,206,86,1)',
+                                hidden: !selectedMetrics.accel_x,
+                            },
+                            {
+                                label: 'Acceleration Y',
+                                data: data.accel_y,
+                                borderColor: 'rgba(75,192,192,1)',
+                                hidden: !selectedMetrics.accel_y,
+                            },
+                            {
+                                label: 'Acceleration Z',
+                                data: data.accel_z,
+                                borderColor: 'rgba(153,102,255,1)',
+                                hidden: !selectedMetrics.accel_z,
+                            },
+                            {
+                                label: 'Battery',
+                                data: data.battery,
+                                borderColor: 'rgba(255,159,64,1)',
+                                hidden: !selectedMetrics.battery,
+                            },
+                        ]}
+                        width={500}
+                        height={300}
+                    />
+                </>
+            ) : (
+                <Typography>Loading...</Typography>
+            )}
+        </Box>
+    );
 };
 
 export default Overview;
